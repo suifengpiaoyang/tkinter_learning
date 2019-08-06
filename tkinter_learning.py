c036e6e64b90cda31460d0316202ed20f2c6e76e
Zhang <504947985@qq.com>
2019-08-06 14:39:45 +0800

    add entry widget

--- tkinter_learning.py
+++ tkinter_learning.py
@@ -9,15 +9,22 @@ win.title('Python GUI')
 
 # 相关函数
 def click_me():
-    button1.configure(text = '** I have been Clicked! **')
-    label1.configure(foreground = 'red')
+    button1.configure(text = 'Hello ' + name.get())
 
 # 添加标签
-label1 = ttk.Label(win,text = 'A Label')
+label1 = ttk.Label(win,text = 'Enter a name:')
 label1.grid(row = 0,column = 0)
 
 # 添加按钮
 button1 = ttk.Button(win,text = 'Click Me!',command = click_me)
-button1.grid(row = 0,column = 1)
+button1.grid(row = 1,column = 1)
+
+# 添加输入框
+name = tk.StringVar()
+entry1 = ttk.Entry(win,width = 12,textvariable = name)
+entry1.grid(row = 1,column = 0)
+
+# 设置光标一开始就在输入框内
+entry1.focus()
 
 win.mainloop()
