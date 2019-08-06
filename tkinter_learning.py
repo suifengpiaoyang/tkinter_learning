a0e90e4f444e3ed995ec8f6f389143514db702a7
Zhang <504947985@qq.com>
2019-08-06 15:02:21 +0800

    add combobox widget

--- tkinter_learning.py
+++ tkinter_learning.py
@@ -10,6 +10,9 @@ win.title('Python GUI')
 # 相关函数
 def click_me():
     button1.configure(text = 'Hello ' + name.get() + ' ' + num.get())
+    print(check_button_flag_1.get(),type(check_button_flag_1.get()))
+    print(check_button_flag_2.get(),type(check_button_flag_2.get()))
+    print(check_button_flag_3.get(),type(check_button_flag_3.get()))
 
 # 添加标签
 label1 = ttk.Label(win,text = 'Enter a name:')
@@ -32,6 +35,22 @@ combobox1 = ttk.Combobox(win,width = 12,textvariable = num)
 combobox1['values'] = (1,2,4,42,100)
 combobox1.grid(row = 1,column = 1)
 combobox1.current(0)
+
+# 添加复选按钮
+check_button_flag_1 = tk.IntVar()
+check1 = tk.Checkbutton(win,text = 'Disabled',variable = check_button_flag_1,state = 'disabled')
+check1.select()
+check1.grid(row = 2,column = 0)
+
+check_button_flag_2 = tk.IntVar()
+check2 = tk.Checkbutton(win,text = 'UnChecked',variable = check_button_flag_2)
+check2.deselect()
+check2.grid(row = 2,column = 1)
+
+check_button_flag_3 = tk.IntVar()
+check3 = tk.Checkbutton(win,text = 'Enabled',variable = check_button_flag_3)
+check3.select()
+check3.grid(row = 2,column = 2)
 
 # 设置光标一开始就在输入框内
 entry1.focus()
